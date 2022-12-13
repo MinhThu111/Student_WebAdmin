c9c159c20df3461be85ce4ca0b600a57d9272a4f
MinhThu2001 <120347883+MinhThu111@users.noreply.github.com>
2022-12-13 11:10:32 +0700

    create form add student

---
+++ WebAdmin/wwwroot/controller/profile_account.js
@@ -0,0 +1,17 @@
+﻿
+
+//Show edit modal
+function ShowEditModal(elm, id) {
+    let text = $(elm).html();
+    $(elm).attr('disabled', true); $(elm).html(_loadAnimationSmallHtml);
+    $.get(`/Account/UserProfile/${id}`).done(function (response) {
+        $(elm).attr('disabled', false); $(elm).html(text);
+        if (response.result === 0 || response.result === -1) {
+            CheckResponseIsSuccess(response); return false;
+        }
+    }).fail(function (err) {
+        $(elm).attr('disabled', false); $(elm).html(text);
+        CheckResponseIsSuccess({ result: -1, error: { code: err.status } });
+    });
+}
+
